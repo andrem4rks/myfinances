5a31ab5f260598ea305c03837e31a74227c540a5
Andrẽ Marks <andrem4rks@gmail.com>
2022-11-21 16:37:43 -0500

    Generate Getters and Setters

--- src/main/java/com/marks/myfinances/models/Lancamento.java
+++ src/main/java/com/marks/myfinances/models/Lancamento.java
@@ -4,6 +4,7 @@ import com.sun.istack.NotNull;
 import org.hibernate.annotations.Check;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Lancamento {
@@ -36,5 +37,83 @@ public class Lancamento {
 
   @OneToOne
   private Usuario usuario;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Integer getMes() {
+    return mes;
+  }
+
+  public void setMes(Integer mes) {
+    this.mes = mes;
+  }
+
+  public Integer getAno() {
+    return ano;
+  }
+
+  public void setAno(Integer ano) {
+    this.ano = ano;
+  }
+
+  public Double getValor() {
+    return valor;
+  }
+
+  public void setValor(Double valor) {
+    this.valor = valor;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public Usuario getUsuario() {
+    return usuario;
+  }
+
+  public void setUsuario(Usuario usuario) {
+    this.usuario = usuario;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Lancamento that = (Lancamento) o;
+    return id == that.id && descricao.equals(that.descricao) && mes.equals(that.mes) && ano.equals(that.ano) && valor.equals(that.valor) && tipo.equals(that.tipo) && status.equals(that.status) && usuario.equals(that.usuario);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, descricao, mes, ano, valor, tipo, status, usuario);
+  }
 
 }
--- src/main/java/com/marks/myfinances/models/Usuario.java
+++ src/main/java/com/marks/myfinances/models/Usuario.java
@@ -1,7 +1,11 @@
 package com.marks.myfinances.models;
 
+import net.bytebuddy.implementation.bind.annotation.Default;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 public class Usuario {
@@ -19,5 +23,59 @@ public class Usuario {
   @Column(length = 20)
   private String senha;
 
-  private Date date;
+  @CreationTimestamp
+  private Date data_cadastro;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public Date getData_cadastro() {
+    return data_cadastro;
+  }
+
+  public void setData_cadastro(Date data_cadastro) {
+    this.data_cadastro = data_cadastro;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Usuario usuario = (Usuario) o;
+    return id.equals(usuario.id) && nome.equals(usuario.nome) && email.equals(usuario.email) && senha.equals(usuario.senha) && data_cadastro.equals(usuario.data_cadastro);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, nome, email, senha, data_cadastro);
+  }
 }
